65ec2779cadeacbd7ba74ac4033ed93acf89e830
Lucas Berthet <lucasberthet@MacBook-Pro-de-Lucas-2.local>
2025-05-09 21:42:45 +0200

    add welcom screen and apply prettier

--- labyrinth.ino
+++ labyrinth.ino
@@ -7,7 +7,7 @@ struct Point
   int y;
 };
 
-struct Element 
+struct Element
 {
   int id;
   Point position;
@@ -15,24 +15,22 @@ struct Element
   int height;
 };
 
-
-
 // GAME DATA
 const int MAX_LEVEL = 10;
 int currentId = 0;
 int currentLevel = 0;
 bool levelEnd = false;
+bool gameStarted = false;
 
 // PLAYER DATA
 const int PLAYER_HEIGHT = 3;
 const int PLAYER_WIDTH = 3;
 
 Element player = {
-  ++currentId,
-  { (gb.display.width() - PLAYER_WIDTH / 2) / 2, (gb.display.height() - PLAYER_HEIGHT / 2) / 2},
-  PLAYER_WIDTH,
-  PLAYER_HEIGHT
-};
+    ++currentId,
+    {(gb.display.width() - PLAYER_WIDTH / 2) / 2, (gb.display.height() - PLAYER_HEIGHT / 2) / 2},
+    PLAYER_WIDTH,
+    PLAYER_HEIGHT};
 
 // MAP DATA
 
@@ -58,16 +56,22 @@ Element rocks[MAX_ROCKS];
 // GAME FUNCTIONS
 void setup()
 {
+  gameStarted = true;
   gb.begin();
   initLevel();
 }
 
 void loop()
 {
-  while (!gb.update());
+  while (!gb.update())
+    ;
   gb.display.clear();
 
-  if (levelEnd)
+  if (gameStarted)
+  {
+    displayWelcomeScreen();
+  }
+  else if (levelEnd)
   {
     displayWinScreen();
   }
@@ -82,24 +86,35 @@ void loop()
 
 void input()
 {
-  if(gb.buttons.repeat(BUTTON_A, 0)) {
-    if (gb.buttons.repeat(BUTTON_LEFT, 0)) push(1, player);
-    else if (gb.buttons.repeat(BUTTON_UP, 0)) push(2, player);
-    else if (gb.buttons.repeat(BUTTON_RIGHT, 0)) push(3, player);
-    else if (gb.buttons.repeat(BUTTON_DOWN, 0)) push(4, player);
+  if (gb.buttons.repeat(BUTTON_A, 0))
+  {
+    if (gb.buttons.repeat(BUTTON_LEFT, 0))
+      push(1, player);
+    else if (gb.buttons.repeat(BUTTON_UP, 0))
+      push(2, player);
+    else if (gb.buttons.repeat(BUTTON_RIGHT, 0))
+      push(3, player);
+    else if (gb.buttons.repeat(BUTTON_DOWN, 0))
+      push(4, player);
   }
 
-  if (gb.buttons.repeat(BUTTON_LEFT, 0)) player.position = move(1, player);
-  else if (gb.buttons.repeat(BUTTON_UP, 0)) player.position = move(2, player);
-  else if (gb.buttons.repeat(BUTTON_RIGHT, 0)) player.position = move(3, player);
-  else if (gb.buttons.repeat(BUTTON_DOWN, 0)) player.position = move(4, player);
+  if (gb.buttons.repeat(BUTTON_LEFT, 0))
+    player.position = move(1, player);
+  else if (gb.buttons.repeat(BUTTON_UP, 0))
+    player.position = move(2, player);
+  else if (gb.buttons.repeat(BUTTON_RIGHT, 0))
+    player.position = move(3, player);
+  else if (gb.buttons.repeat(BUTTON_DOWN, 0))
+    player.position = move(4, player);
 
-  if(gb.buttons.pressed(BUTTON_B)) {
+  if (gb.buttons.pressed(BUTTON_B))
+  {
     initLevel();
   }
 }
 
-void update() {
+void update()
+{
   fillHoles();
 }
 
@@ -129,22 +144,48 @@ void checkWinCondition()
 void displayWinScreen()
 {
   gb.display.clear();
-  gb.display.setColor(WHITE);
-  gb.display.println("");
-  gb.display.println("  FELICITATIONS!");
-  gb.display.println("");
-  gb.display.println("  Vous avez fini");
-  gb.display.println("  le labyrinthe!");
-  gb.display.println("");
-  gb.display.println("  Appuyez sur A");
-  gb.display.println("  pour continuer");
+  gb.display.setColor(YELLOW);
+  gb.display.setCursor(10, 5);
+  gb.display.println("FELICITATIONS!");
+  gb.display.setCursor(5, 20);
+  gb.display.println("Vous avez fini le");
+  gb.display.setCursor(5, 30);
+  gb.display.println("labyrinthe !");
+  gb.display.setColor(GREEN);
+  gb.display.setCursor(5, 50);
+  gb.display.println("Appuyez sur A pour continuer");
 
   if (gb.buttons.pressed(BUTTON_A))
   {
     currentLevel++;
     levelEnd = false;
-    if(currentLevel < MAX_LEVEL) {
+    if (currentLevel < MAX_LEVEL)
+    {
       initLevel();
     }
+  }
+}
+
+void displayWelcomeScreen()
+{
+  gb.display.clear();
+
+  gb.display.setColor(YELLOW);
+  gb.display.setCursor(10, 5);
+  gb.display.println("LABYRINTHE");
+
+  gb.display.setColor(WHITE);
+  gb.display.setCursor(5, 20);
+  gb.display.println("CROIX: DEPLAC.");
+  gb.display.setCursor(5, 30);
+  gb.display.println("A+CROIX: POUSSER");
+
+  gb.display.setColor(GREEN);
+  gb.display.setCursor(5, 40);
+  gb.display.println("A: JOUER");
+
+  if (gb.buttons.pressed(BUTTON_A))
+  {
+    gameStarted = false;
   }
 }
--- rock.ino
+++ rock.ino
@@ -1,14 +1,17 @@
-void initRocks() {
+void initRocks()
+{
     int found = 0;
-    for(int y = 0; y < GRID_SIZE; y++) {
-        for(int x = 0; x < GRID_SIZE; x++) {
-            if(pattern[y][x] == 2) {
+    for (int y = 0; y < GRID_SIZE; y++)
+    {
+        for (int x = 0; x < GRID_SIZE; x++)
+        {
+            if (pattern[y][x] == 2)
+            {
                 rocks[found] = {
                     ++currentId,
-                    { x * TILE_WIDTH, y * TILE_HEIGHT },
+                    {x * TILE_WIDTH, y * TILE_HEIGHT},
                     ROCK_SIZE,
-                    ROCK_SIZE
-                };
+                    ROCK_SIZE};
                 pattern[y][x] = 0;
                 found++;
             }
@@ -16,12 +19,12 @@ void initRocks() {
     }
 }
 
-bool isRockAhead(Point position, Element element) {
+bool isRockAhead(Point position, Element element)
+{
     bool isRock = false;
-    for(int i = 0; i < MAX_ROCKS; i++) {
-        if(rocks[i].width > 0 
-        && gb.collide.rectRect(position.x, position.y, element.width, element.height, rocks[i].position.x, rocks[i].position.y, rocks[i].width, rocks[i].height) 
-        && element.id != rocks[i].id)
+    for (int i = 0; i < MAX_ROCKS; i++)
+    {
+        if (rocks[i].width > 0 && gb.collide.rectRect(position.x, position.y, element.width, element.height, rocks[i].position.x, rocks[i].position.y, rocks[i].width, rocks[i].height) && element.id != rocks[i].id)
         {
             isRock = true;
             break;
@@ -30,9 +33,11 @@ bool isRockAhead(Point position, Element element) {
     return isRock;
 }
 
-Element pushRock(Point position, Element element, int direction) {
-    for(int i = 0; i < MAX_ROCKS; i++) {
-        if(gb.collide.rectRect(position.x, position.y, element.width, element.height, rocks[i].position.x, rocks[i].position.y, rocks[i].width, rocks[i].height))
+Element pushRock(Point position, Element element, int direction)
+{
+    for (int i = 0; i < MAX_ROCKS; i++)
+    {
+        if (gb.collide.rectRect(position.x, position.y, element.width, element.height, rocks[i].position.x, rocks[i].position.y, rocks[i].width, rocks[i].height))
         {
             rocks[i].position = move(direction, rocks[i]);
             break;
@@ -40,31 +45,36 @@ Element pushRock(Point position, Element element, int direction) {
     }
 }
 
-bool isInHole(Element element) {
-    return getPointValue(element.position.x, element.position.y) == 3 
-        && getPointValue(element.position.x + element.width - 1, element.position.y) == 3
-        && getPointValue(element.position.x + element.width - 1, element.position.y + element.height - 1) == 3
-        && getPointValue(element.position.x, element.position.y + element.height - 1) == 3;
+bool isInHole(Element element)
+{
+    return getPointValue(element.position.x, element.position.y) == 3 && getPointValue(element.position.x + element.width - 1, element.position.y) == 3 && getPointValue(element.position.x + element.width - 1, element.position.y + element.height - 1) == 3 && getPointValue(element.position.x, element.position.y + element.height - 1) == 3;
 }
 
-void fillHoles() {
-    for(int i = 0; i < MAX_ROCKS; i++) {
-        if(isInHole(rocks[i])) {
+void fillHoles()
+{
+    for (int i = 0; i < MAX_ROCKS; i++)
+    {
+        if (isInHole(rocks[i]))
+        {
             pattern[rocks[i].position.y / TILE_HEIGHT][rocks[i].position.x / TILE_WIDTH] = FLOOR;
-            rocks[i].width = 0;            
+            rocks[i].width = 0;
         }
     }
 }
 
-void drawRocks() {
-    for(int i = 0; i < MAX_ROCKS; i++) {
-        if(rocks[i].width != 0) {
+void drawRocks()
+{
+    for (int i = 0; i < MAX_ROCKS; i++)
+    {
+        if (rocks[i].width != 0)
+        {
             drawRock(rocks[i]);
         }
     }
 }
 
-void drawRock(Element rock) {
+void drawRock(Element rock)
+{
     gb.display.setColor(DARKGRAY);
     gb.display.fillRect(rock.position.x, rock.position.y, rock.width, rock.height);
 }
